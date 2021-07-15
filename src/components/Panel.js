489a2d6d2bbedd3552ac3fed95be1bb5bfa935c3
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-15 10:39:51 +0100

    Update Panel.js

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,6 +5,22 @@ import Quote from "./Quote";
 
 const Panel = ({ handleColorChange }) => {
   const [color, setColor] = useState("purple");
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    getQuote();
+  }, [quote]);
+
+  const getQuote = async () => {
+    let url =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+    let response = await fetch(url);
+    let result = await response.json();
+    setQuote(result);
+    console.log(result);
+  };
+
   useEffect(() => {
     handleColorChange(color);
   });
